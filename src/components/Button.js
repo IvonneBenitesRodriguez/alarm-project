87a0239389e98a18269077636c1281f6bffbe48e
Ivonne <ivonnebenites2015@gmail.com>
2023-10-29 20:10:38 -0500

    Add folder named components to store jsx files

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SetAlarmButton({ onClick }) {
+  return (
+    <button onClick={onClick}>Set Alarm</button>
+  );
+}
+
+export default SetAlarmButton;
---
+++ src/components/Clock.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+
+function Clock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const hours = time.getHours().toString().padStart(2, '0');
+  const minutes = time.getMinutes().toString().padStart(2, '0');
+  const seconds = time.getSeconds().toString().padStart(2, '0');
+
+  return (
+    <div className="clock">
+      <div className="time">
+        {hours}
+        :
+        {minutes}
+        :
+        {seconds}
+      </div>
+    </div>
+  );
+}
+
+export default Clock;
